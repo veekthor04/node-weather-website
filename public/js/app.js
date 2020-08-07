7a3ce341cd8f39e828b5858e0ff7db7e12e171ca
veekthor04 <victoradenuga04@yahoo.com>
2020-08-07 03:43:47 +0100

    remove unnecessary

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,7 @@
-console.log('Client side javascript is loaded');
-
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
